ec2320993fae2dd1d757950d40c62b2f444ed63f
Adam Eivy <atomantic@gmail.com>
2018-10-19 06:49:16 -0700

    7.0.2: update all deps

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const _ = require('lodash')
+const {merge} = require('lodash')
 const config = require('./lib/config')
 
 module.exports = {
     pkg: require('./package.json'),
     register: async function (server, opts) {
         // merge custom options over default config
-        const opt = _.merge(config, opts)
+        const opt = merge(config, opts)
     
         // allow custom objects to be passed by reference
         // lest _.merge copy the data at time of passing it
