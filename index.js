b28b80f8e5f3f6b13534ab865b05d8afd32d6d99
Adam Eivy <atomantic@gmail.com>
2018-10-19 07:46:56 -0700

    bring back support for Node 4 and test 8,10 in travis

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const {merge} = require('lodash')
+const _ = require('lodash')
 const config = require('./lib/config')
 
 module.exports = {
     pkg: require('./package.json'),
     register: async function (server, opts) {
         // merge custom options over default config
-        const opt = merge(config, opts)
+        const opt = _.merge(config, opts)
     
         // allow custom objects to be passed by reference
         // lest _.merge copy the data at time of passing it
